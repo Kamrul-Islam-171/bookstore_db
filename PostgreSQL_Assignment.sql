03f422e4eda25ea6c6f8b21aadc3bc20dee99a22
Kamrul-Islam-171 <kamrul.ruet.171@gmail.com>
2025-04-05 21:28:45 +0600

    all tables are created

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -18,3 +18,38 @@ INSERT INTO books (title, author, price, stock, published_year) VALUES
 ('Database Design Principles', 'Jane Smith', 20.00, 0, 2018);
 
 SELECT * FROM books;
+
+CREATE TABLE customers (
+    id SERIAL PRIMARY KEY,
+    "name" VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    joined_date DATE DEFAULT CURRENT_DATE
+);
+
+drop TABLE customers;
+
+INSERT INTO customers ("name", email, joined_date) VALUES
+('Alice', 'alice@email.com', '2023-01-10'),
+('Bob', 'bob@email.com', '2022-05-15'),
+('Charlie', 'charlie@email.com', '2023-06-20');
+INSERT INTO customers ("name", email) VALUES
+('Alita', 'alita@email.com');
+
+
+SELECT * FROM customers;
+
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    customer_id INT REFERENCES customers(id) on DELETE CASCADE,
+    book_id INT REFERENCES books(id) on DELETE CASCADE,
+    quantity INT CHECK (quantity > 0),
+    order_date TIMESTAMP DEFAULT current_timestamp
+);
+
+INSERT INTO orders (customer_id, book_id, quantity, order_date) VALUES
+(1, 2, 1, '2024-03-10'),
+(2, 1, 1, '2024-02-20'),
+(1, 3, 2, '2024-03-05');
+
+
+SELECT * FROM orders;
